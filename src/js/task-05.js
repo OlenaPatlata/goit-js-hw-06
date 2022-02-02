551f48fe386585ebdd22c749c90c282d544947af
OlenaPatlata <94738131+OlenaPatlata@users.noreply.github.com>
2022-02-02 18:02:41 +0200

    Update task-05.js

---
+++ src/js/task-05.js
@@ -0,0 +1,18 @@
+// Задание 5
+// Напиши скрипт который, при наборе текста в инпуте input#name-input (событие input), подставляет его текущее значение в span#name-output. Если инпут пустой, в спане должна отображаться строка "Anonymous".
+
+// Получили ссылки на элементы
+const refs = {
+    inputRef: document.querySelector('#name-input'),
+    spanRef: document.querySelector('#name-output'),
+}
+
+// Объявили переменную с дефолтным значением для приветствия
+let defoltValue = "Anonymous"
+
+// Объявляем стрелочную функцию, результатом вызова которой есть замена дефолтного зачения приветствия после введения пользователем имени в input
+const addText = () => !refs.inputRef.value ? refs.spanRef.textContent = defoltValue : refs.spanRef.textContent = refs.inputRef.value;
+
+// Вешаем слушателя событий на input и в качестве колбека передаем стрелочную функцию
+refs.inputRef.addEventListener('input', addText)
+
